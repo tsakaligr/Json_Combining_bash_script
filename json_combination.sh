6918180178426da86298c0ad061822192cb542b4
Georgios Kafanas <g.kafanas@greeklug.gr>
2022-11-02 19:20:58 +0200

    Prepare script for execution in UNIX
    - Convert DOS new lines to UNIX.
    - Make the script executable.

--- json_combination.sh
+++ json_combination.sh
@@ -5,7 +5,7 @@
 #it makes a .json file called all.json as an output
 
 echo "," | tee -a *.json
-sed -i '$ s/.$/ /' $2
+sed -i '$s/.$/ /' $2
 echo "]" | tee -a $2
 sed -i '1s/^/[\n/' $1
 cat *.json > all.json
