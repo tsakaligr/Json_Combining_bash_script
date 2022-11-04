4f3fbfb54ccf519f7e25e7c6711141435177cb02
Georgios Kafanas <g.kafanas@greeklug.gr>
2022-11-04 10:21:07 +0200

    Create batch script which combines all JSON files in a directory
    - Avoid modifying the input files, but create a stream to process each
      file.
    - Use an array to store the files that will be processed.

---
+++ json_files_combine.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script combines .json files
+#
+# Input [1]: directory with .json files
+# Output: json file in the stdout 
+
+declare directory="${1}"
+
+declare -a files=()
+readarray -d '' files < <(find "${directory}" -type f -print0 | sort -z)
+
+# In a given file '[' can be inserted with: sed '1i \['
+echo "["
+
+for f in "${files[@]:0:${#files[@]}-1}"; do
+	cat "${f}" | sed '$s/$/,/'
+done
+
+if [ "${#files[@]}" -gt 0 ]; then
+	cat "${files[-1]}" | sed '$s/$//'
+fi
+
+# In a given file ']' can be inserted with: sed '$a \]'
+echo "]"
+
+exit 0
