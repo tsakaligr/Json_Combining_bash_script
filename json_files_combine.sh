8f472d70d41ee60712daabf076db8570cb33ad01
NikosPC <neorisionick@greeklug.gr>
2022-11-12 12:48:53 +0200

    Extract function for processing json files

--- json_files_combine.sh
+++ json_files_combine.sh
@@ -7,19 +7,25 @@
 
 declare directory="${1}"
 
+seperate_json_entries() {
+        local -n file_list="${1}"
+
+        for f in "${file_list[@]:0:${#file_list[@]}-1}"; do
+                cat "${f}" | sed '$s/$/,/'
+        done
+
+        if [ "${#file_list[@]}" -gt 0 ]; then
+                cat "${file_list[-1]}" | sed '$s/$//'
+        fi
+}
+
 declare -a files=()
 readarray -d '' files < <(find "${directory}" -type f -print0 | sort -z)
 
 # In a given file '[' can be inserted with: sed '1i \['
 echo "["
 
-for f in "${files[@]:0:${#files[@]}-1}"; do
-	cat "${f}" | sed '$s/$/,/'
-done
-
-if [ "${#files[@]}" -gt 0 ]; then
-	cat "${files[-1]}" | sed '$s/$//'
-fi
+seperate_json_entries 'files'
 
 # In a given file ']' can be inserted with: sed '$a \]'
 echo "]"
